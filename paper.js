2d85f01810cf3f0a9fbd9389ef03ef6f04b90cff
RaghavTaneja99 <78413994+RaghavTaneja99@users.noreply.github.com>
2021-05-21 18:30:00 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class paper{
+    constructor(x,y){
+        var options = {
+            'restitution':0.8,
+            'friction':20,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, 50, 50, options);
+        this.width = width;
+        this.height = height;
+        this.image=loadImage("paper.png")
+        World.add(world,this.body)
+    }
+    display(){
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x,this.body.position.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image,0, 0, 50, 50);
+    pop();
+    }
+}
